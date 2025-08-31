de68b1ab71aab103fabf2d46d30236ef7f07d4b1
Vedarghya Kundu <vedarghya05@outlook.com>
2025-08-31 23:44:11 +0530

    added the vercel app domain to main.py backend file

--- Backend/main.py
+++ Backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://code-prep-omega.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
